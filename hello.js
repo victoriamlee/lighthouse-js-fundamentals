5d573d8ec7c4064427fe47cb87090c37611ba68d
Victoria Lee <victorialee72@live.com>
2020-07-04 02:00:47 +0000

    Working with functions

---
+++ hello.js
@@ -0,0 +1,13 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
